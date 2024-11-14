cf384e0cfa42ce084388c8cef990289f34516032
names1031@stud.uni-obuda.hu <names1031@stud.uni-obuda.hu>
2024-11-14 13:12:28 +0100

    Added captain model

---
+++ Cotd_Editor/Models/Captains/CaptainModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Cotd_Editor.Models.Captains;
+
+public class CaptainModel
+{
+	public string Image { get; set; } = string.Empty;
+	public string Name { get; set; } = string.Empty;
+	public int Health { get; set; } = 0;
+	public int Power { get; set; } = 0;
+	public int Armor { get; set; } = 0;
+}
